b676ca226eeaf5039dff17591084465da6e4b52b
ssbalashov <ss.balashov@gmail.com>
2020-11-10 15:00:19 +0500

    Updated amountLabel

--- Bank App/ViewController.swift
+++ Bank App/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountLabel: UILabel?
+    
+    var amount = 9999.99 {
+        didSet {
+            updateAmountLabel()
+        }
+    }
+    
+    func updateAmountLabel() {
+        amountLabel?.text = "$ \(amount)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateAmountLabel()
     }
 
 
